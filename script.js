52cf6a11d6cc3512dbf18a0d38c5ea858e38f830
AlexHetzelfde <mightygamer500@gmail.com>
2026-03-12 14:52:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const scroller = scrollama();
 
-  const textLayer = document.getElementById("text-layer");
-  const backgroundLayer = document.getElementById("background-layer");
-  const steps = document.querySelectorAll(".step");
-
-  let maxScroll = window.innerHeight * steps.length;
-
-  // Scrollama setup
   scroller
     .setup({
       step: ".step",
@@ -18,19 +10,5 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Step:", response.index);
     });
 
-  // update bij resize
-  window.addEventListener("resize", () => {
-    maxScroll = window.innerHeight * steps.length;
-  });
-
-  // PARALLAX SCROLL
-  window.addEventListener("scroll", () => {
-
-    const scrollY = window.scrollY;
-
-    textLayer.style.transform = `translateY(${-scrollY}px)`;
-    backgroundLayer.style.transform = `translateY(${-scrollY * 0.6}px)`;
-
-  });
-
+  window.addEventListener("resize", scroller.resize);
 });
